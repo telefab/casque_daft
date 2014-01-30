9635a14aedb97cbff6858003542145a34b38e0c1
nvcs <nicolas.szlifierski@gmail.com>
2014-01-30 17:38:16 +0100

    code

---
+++ code.c
@@ -0,0 +1,629 @@
+int arrow[] = {0,0,0,1,0,0,0,
+			   0,0,1,1,1,0,0,
+			   0,1,1,0,1,1,0,
+			   1,1,0,0,0,1,1,
+			   1,0,0,0,0,0,1};
+
+int arrow_r[] = {1,0,0,0,0,0,1,
+				 1,1,0,0,0,1,1,
+				 0,1,1,0,1,1,0,
+				 0,0,1,1,1,0,0,
+				 0,0,0,1,0,0,0};
+
+int char_a[] = {0,0,0,0,0,0,0,
+						0,0,1,1,1,1,0,
+						0,1,0,1,0,0,0,
+						0,0,1,1,1,1,0,
+						0,0,0,0,0,0,0};
+
+int char_b[] = {0,0,0,0,0,0,0,
+						0,0,1,0,1,0,0,
+						0,1,0,1,0,1,0,
+						0,1,1,1,1,1,0,
+						0,0,0,0,0,0,0};
+
+int char_c[] = {0,0,0,0,0,0,0,
+						0,1,0,0,0,1,0,
+						0,1,0,0,0,1,0,
+						0,0,1,1,1,0,0,
+						0,0,0,0,0,0,0};
+
+int char_d[] = {0,0,0,0,0,0,0,
+						0,0,1,1,1,0,0,
+						0,1,0,0,0,1,0,
+						0,1,1,1,1,1,0,
+						0,0,0,0,0,0,0};
+
+int char_e[] = {0,0,0,0,0,0,0,
+						0,1,0,0,0,1,0,
+						0,1,0,1,0,1,0,
+						0,1,1,1,1,1,0,
+						0,0,0,0,0,0,0};
+
+int char_f[] = {0,0,0,0,0,0,0,
+						0,1,0,0,0,0,0,
+						0,1,0,1,0,0,0,
+						0,1,1,1,1,1,0,
+						0,0,0,0,0,0,0};
+
+int char_g[] = {0,0,0,0,0,0,0,
+						0,0,0,1,1,1,0,
+						0,1,0,0,0,1,0,
+						0,0,1,1,1,0,0,
+						0,0,0,0,0,0,0};
+
+int char_h[] = {0,0,0,0,0,0,0,
+						0,1,1,1,1,1,0,
+						0,0,0,1,0,0,0,
+						0,1,1,1,1,1,0,
+						0,0,0,0,0,0,0};
+
+int char_i[] = {0,0,0,0,0,0,0,
+						0,1,0,0,0,1,0,
+						0,1,1,1,1,1,0,
+						0,1,0,0,0,1,0,
+						0,0,0,0,0,0,0};
+
+int char_j[] = {0,0,0,0,0,0,0,
+						0,1,1,1,1,0,0,
+						0,1,0,0,0,1,0,
+						0,1,0,0,0,1,0,
+						0,0,0,0,0,0,0};
+
+int char_k[] = {0,0,0,0,0,0,0,
+						0,1,1,0,1,1,0,
+						0,0,0,1,0,0,0,
+						0,1,1,1,1,1,0,
+						0,0,0,0,0,0,0};
+
+int char_l[] = {0,0,0,0,0,0,0,
+						0,0,0,0,0,1,0,
+						0,0,0,0,0,1,0,
+						0,1,1,1,1,1,0,
+						0,0,0,0,0,0,0};
+
+int char_m[] = {0,0,0,0,0,0,0,
+						0,1,1,1,1,1,0,
+						0,0,1,0,0,0,0,
+						0,1,1,1,1,1,0,
+						0,0,0,0,0,0,0};
+
+int char_n[] = {0,0,0,0,0,0,0,
+						0,1,1,1,1,1,0,
+						0,0,1,1,1,0,0,
+						0,1,1,1,1,1,0,
+						0,0,0,0,0,0,0};
+
+int char_o[] = {0,0,0,0,0,0,0,
+						0,1,1,1,1,1,0,
+						0,1,0,0,0,1,0,
+						0,1,1,1,1,1,0,
+						0,0,0,0,0,0,0};
+
+int char_p[] = {0,0,0,0,0,0,0,
+						0,0,1,0,0,0,0,
+						0,1,0,1,0,0,0,
+						0,1,1,1,1,1,0,
+						0,0,0,0,0,0,0};
+
+int char_q[] = {0,0,0,0,0,0,0,
+						0,1,1,1,0,0,0,
+						0,1,0,1,1,0,0,
+						0,1,1,1,0,1,0,
+						0,0,0,0,0,0,0};
+
+int char_r[] = {0,0,0,0,0,0,0,
+						0,0,1,0,0,1,0,
+						0,1,0,1,1,0,0,
+						0,1,1,1,1,1,0,
+						0,0,0,0,0,0,0};
+
+int char_s[] = {0,0,0,0,0,0,0,
+						0,1,0,0,1,0,0,
+						0,1,0,1,0,1,0,
+						0,0,1,0,0,1,0,
+						0,0,0,0,0,0,0};
+
+int char_t[] = {0,0,0,0,0,0,0,
+						0,1,0,0,0,0,0,
+						0,1,1,1,1,1,0,
+						0,1,0,0,0,0,0,
+						0,0,0,0,0,0,0};
+
+int char_u[] = {0,0,0,0,0,0,0,
+						0,1,1,1,1,1,0,
+						0,0,0,0,0,1,0,
+						0,1,1,1,1,1,0,
+						0,0,0,0,0,0,0};
+
+int char_v[] = {0,0,0,0,0,0,0,
+						0,1,1,1,0,0,0,
+						0,0,0,0,1,1,0,
+						0,1,1,1,0,0,0,
+						0,0,0,0,0,0,0};
+
+int char_w[] = {0,0,0,0,0,0,0,
+						0,1,1,1,1,1,0,
+						0,0,0,0,1,0,0,
+						0,1,1,1,1,1,0,
+						0,0,0,0,0,0,0};
+
+int char_x[] = {0,0,0,0,0,0,0,
+						0,1,1,0,1,1,0,
+						0,0,0,1,0,0,0,
+						0,1,1,0,1,1,0,
+						0,0,0,0,0,0,0};
+
+int char_y[] = {0,0,0,0,0,0,0,
+						0,1,1,0,0,0,0,
+						0,0,0,1,0,0,0,
+						0,1,1,0,1,1,0,
+						0,0,0,0,0,0,0};
+
+int char_z[] = {0,0,0,0,0,0,0,
+						0,1,1,0,0,1,0,
+						0,1,0,1,0,1,0,
+						0,1,0,0,1,1,0,
+						0,0,0,0,0,0,0};
+
+int char_null[] = {0,0,0,0,0,0,0,
+						0,0,0,0,0,0,0,
+						0,0,0,0,0,0,0,
+						0,0,0,0,0,0,0,
+						0,0,0,0,0,0,0};
+
+int dispers_1[] = {0,0,0,0,0,0,0,
+				   0,0,0,0,0,0,0,
+				   0,0,0,1,0,0,0,
+				   0,0,0,0,0,0,0,
+				   0,0,0,0,0,0,0};
+
+int dispers_2[] = {0,0,0,0,0,0,0,
+				   0,0,0,1,0,0,0,
+				   0,0,1,1,1,0,0,
+				   0,0,0,1,0,0,0,
+				   0,0,0,0,0,0,0};
+
+int dispers_3[] = {0,0,0,1,0,0,0,
+				   0,0,1,1,1,0,0,
+				   0,1,1,0,1,1,0,
+				   0,0,1,1,1,0,0,
+				   0,0,0,1,0,0,0};
+
+int dispers_4[] = {0,0,1,1,1,0,0,
+				   0,1,1,0,1,1,0,
+				   1,1,0,0,0,1,1,
+				   0,1,1,0,1,1,0,
+				   0,0,1,1,1,0,0};
+
+int dispers_5[] = {0,1,1,0,1,1,0,
+				   1,1,0,0,0,1,1,
+				   1,0,0,0,0,0,1,
+				   1,1,0,0,0,1,1,
+				   0,1,0,0,0,1,0};
+
+int dispers_6[] = {1,1,0,0,0,1,1,
+				   1,0,0,0,0,0,1,
+				   0,0,0,0,0,0,0,
+				   1,0,0,0,0,0,1,
+				   1,1,0,0,0,1,1};
+
+int dispers_7[] = {1,0,0,0,0,0,1,
+				   0,0,0,0,0,0,0,
+				   0,0,0,0,0,0,0,
+				   0,0,0,0,0,0,0,
+				   1,0,0,0,0,0,1};
+
+int dispers_8[] = {0,0,0,0,0,0,0,
+				   0,0,0,0,0,0,0,
+				   0,0,0,0,0,0,0,
+				   0,0,0,0,0,0,0,
+				   0,0,0,0,0,0,0};
+
+int * dispers[] = {dispers_1, dispers_2, dispers_3, dispers_4, dispers_5, dispers_6, dispers_7, dispers_8};
+
+int del_1[] = {7,8,9,10,11,12,13,
+				 2,3,4,5,6};
+
+int del_2[] = {14,15,16,17,18,19,20,
+				 22,24,26,28,30};
+
+int del_3[] = {32,34,36,38,40,42,44,
+				 46,48,50,52,53};
+
+int * del[] = {del_1, del_2, del_3};
+int mode = 0;
+int pinButton = 31;
+int buttonState = LOW;
+
+void setup()
+{
+	pinMode(14, OUTPUT);
+	pinMode(15, OUTPUT);
+	pinMode(16, OUTPUT);
+	pinMode(17, OUTPUT);
+	pinMode(18, OUTPUT);
+	pinMode(19, OUTPUT);
+	pinMode(20, OUTPUT);
+	
+	pinMode(32, OUTPUT);
+	pinMode(34, OUTPUT);
+	pinMode(36, OUTPUT);
+	pinMode(38, OUTPUT);
+	pinMode(40, OUTPUT);
+	pinMode(42, OUTPUT);
+	pinMode(44, OUTPUT);
+	
+	pinMode(13, OUTPUT);
+	pinMode(12, OUTPUT);
+	pinMode(11, OUTPUT);
+	pinMode(10, OUTPUT);
+	pinMode(9, OUTPUT);
+	pinMode(8, OUTPUT);
+	pinMode(7, OUTPUT);
+	
+	pinMode(2, OUTPUT);
+	pinMode(3, OUTPUT);
+	pinMode(4, OUTPUT);
+	pinMode(5, OUTPUT);
+	pinMode(6, OUTPUT);
+	
+	pinMode(46, OUTPUT);
+	pinMode(48, OUTPUT);
+	pinMode(50, OUTPUT);
+	pinMode(52, OUTPUT);
+	pinMode(53, OUTPUT);
+	
+	pinMode(22, OUTPUT);
+	pinMode(24, OUTPUT);
+	pinMode(26, OUTPUT);
+	pinMode(28, OUTPUT);
+	pinMode(30, OUTPUT);
+	
+	pinMode(pinButton, INPUT);
+	
+	randomSeed(analogRead(15));
+	
+	Serial.begin(9600);
+}
+
+void loop()
+{
+
+
+	int * table[] = {char_null, char_null, char_null, char_h, char_a, char_r, char_d, char_e, char_r, char_null, char_b, char_e, char_t, char_t, char_e, char_r, char_null, char_f, char_a, char_s, char_t, char_e, char_r, char_null, char_s, char_t, char_r, char_o, char_n, char_g, char_e, char_r, char_null};
+
+	int letters = 33;
+	
+	
+	int bass = analogRead(A0);
+	int table_shift[35];
+	int table_shift_r[35];
+	
+			
+			
+	if (digitalRead(pinButton) == HIGH && buttonState == LOW)
+	{
+		mode = (mode + 1) % 3;
+		buttonState = HIGH;
+	
+	}
+	else if (digitalRead(pinButton) == LOW && buttonState == HIGH)
+	{
+		buttonState = LOW;
+	}
+	
+	switch(mode) 
+	{
+		case 0:
+			
+			display_shift_words(table, del, letters);
+			break;
+			
+	
+		case 1:
+			
+			if (bass > 300)
+			{
+				restart:
+				for (int i=0; i<13; i++)
+				{
+					for (int j=0; j<8; j++)
+					{
+						if (digitalRead(pinButton) == HIGH && buttonState == LOW)
+						{
+							mode = (mode + 1) % 3;
+							buttonState = HIGH;
+							return;
+						
+						}
+						else if (digitalRead(pinButton) == LOW && buttonState == HIGH)
+						{
+							buttonState = LOW;
+						}
+			
+						int disp = 0;
+						if (i<8) {disp = i;} else {disp = 7;}
+						
+						shift(arrow, table_shift, i-8);
+						
+						shift(arrow_r, table_shift_r, 8-i);
+						
+						if ((i>3) && (analogRead(A0) > 300))
+						{
+							goto restart;
+						}
+						
+						display_complexe(table_shift_r, dispers[disp], table_shift, del_1, del_2, del_3);
+					}
+				}
+			}
+			
+			Serial.print("\nValeur du Capteur = " );
+			Serial.print(bass);
+			break;
+			
+		case 2:
+		
+			Serial.print("\nValeur du Capteur = " );
+			Serial.print(bass);
+			
+			int value = (int) floor((bass/400.0)*7.0);
+			
+			Serial.print(" => " );
+			Serial.print(value);
+			
+			int tab_1[35];
+			int tab_2[35];
+			int tab_3[35];
+			
+			if (value > 7) {value = 7;}
+			
+			if (value > 0)
+			{
+
+				if (value == 7)
+				{
+					for (int i=0; i<15; i++)
+					{
+						int max = value + (int) random(-1,1);
+						
+						if (i < 5)
+						{
+							if (max > 6) {tab_1[0+7*i] = 1;} else {tab_1[0+7*i] = 0;}
+							if (max > 5) {tab_1[1+7*i] = 1;} else {tab_1[1+7*i] = 0;}
+							if (max > 4) {tab_1[2+7*i] = 1;} else {tab_1[2+7*i] = 0;}
+							if (max > 3) {tab_1[3+7*i] = 1;} else {tab_1[3+7*i] = 0;}
+							if (max > 2) {tab_1[4+7*i] = 1;} else {tab_1[4+7*i] = 0;}
+							if (max > 1) {tab_1[5+7*i] = 1;} else {tab_1[5+7*i] = 0;}
+							if (max > 0) {tab_1[6+7*i] = 1;} else {tab_1[6+7*i] = 0;}
+						}
+						if (i < 10)
+						{
+							if (max > 6) {tab_2[0+7*i-35] = 1;} else {tab_2[0+7*i-35] = 0;}
+							if (max > 5) {tab_2[1+7*i-35] = 1;} else {tab_2[1+7*i-35] = 0;}
+							if (max > 4) {tab_2[2+7*i-35] = 1;} else {tab_2[2+7*i-35] = 0;}
+							if (max > 3) {tab_2[3+7*i-35] = 1;} else {tab_2[3+7*i-35] = 0;}
+							if (max > 2) {tab_2[4+7*i-35] = 1;} else {tab_2[4+7*i-35] = 0;}
+							if (max > 1) {tab_2[5+7*i-35] = 1;} else {tab_2[5+7*i-35] = 0;}
+							if (max > 0) {tab_2[6+7*i-35] = 1;} else {tab_2[6+7*i-35] = 0;}
+						}
+						if (i < 15)
+						{
+							if (max > 6) {tab_3[0+7*i-70] = 1;} else {tab_3[0+7*i-70] = 0;}
+							if (max > 5) {tab_3[1+7*i-70] = 1;} else {tab_3[1+7*i-70] = 0;}
+							if (max > 4) {tab_3[2+7*i-70] = 1;} else {tab_3[2+7*i-70] = 0;}
+							if (max > 3) {tab_3[3+7*i-70] = 1;} else {tab_3[3+7*i-70] = 0;}
+							if (max > 2) {tab_3[4+7*i-70] = 1;} else {tab_3[4+7*i-70] = 0;}
+							if (max > 1) {tab_3[5+7*i-70] = 1;} else {tab_3[5+7*i-70] = 0;}
+							if (max > 0) {tab_3[6+7*i-70] = 1;} else {tab_3[6+7*i-70] = 0;}
+						}
+					}
+				}
+				else
+				{
+					for (int i=0; i<15; i++)
+					{
+						int max = value + (int) random(-1,2);
+						
+						if (i < 5)
+						{
+							if (max > 6) {tab_1[0+7*i] = 1;} else {tab_1[0+7*i] = 0;}
+							if (max > 5) {tab_1[1+7*i] = 1;} else {tab_1[1+7*i] = 0;}
+							if (max > 4) {tab_1[2+7*i] = 1;} else {tab_1[2+7*i] = 0;}
+							if (max > 3) {tab_1[3+7*i] = 1;} else {tab_1[3+7*i] = 0;}
+							if (max > 2) {tab_1[4+7*i] = 1;} else {tab_1[4+7*i] = 0;}
+							if (max > 1) {tab_1[5+7*i] = 1;} else {tab_1[5+7*i] = 0;}
+							if (max > 0) {tab_1[6+7*i] = 1;} else {tab_1[6+7*i] = 0;}
+						}
+						if (i < 10)
+						{
+							if (max > 6) {tab_2[0+7*i-35] = 1;} else {tab_2[0+7*i-35] = 0;}
+							if (max > 5) {tab_2[1+7*i-35] = 1;} else {tab_2[1+7*i-35] = 0;}
+							if (max > 4) {tab_2[2+7*i-35] = 1;} else {tab_2[2+7*i-35] = 0;}
+							if (max > 3) {tab_2[3+7*i-35] = 1;} else {tab_2[3+7*i-35] = 0;}
+							if (max > 2) {tab_2[4+7*i-35] = 1;} else {tab_2[4+7*i-35] = 0;}
+							if (max > 1) {tab_2[5+7*i-35] = 1;} else {tab_2[5+7*i-35] = 0;}
+							if (max > 0) {tab_2[6+7*i-35] = 1;} else {tab_2[6+7*i-35] = 0;}
+						}
+						if (i < 15)
+						{
+							if (max > 6) {tab_3[0+7*i-70] = 1;} else {tab_3[0+7*i-70] = 0;}
+							if (max > 5) {tab_3[1+7*i-70] = 1;} else {tab_3[1+7*i-70] = 0;}
+							if (max > 4) {tab_3[2+7*i-70] = 1;} else {tab_3[2+7*i-70] = 0;}
+							if (max > 3) {tab_3[3+7*i-70] = 1;} else {tab_3[3+7*i-70] = 0;}
+							if (max > 2) {tab_3[4+7*i-70] = 1;} else {tab_3[4+7*i-70] = 0;}
+							if (max > 1) {tab_3[5+7*i-70] = 1;} else {tab_3[5+7*i-70] = 0;}
+							if (max > 0) {tab_3[6+7*i-70] = 1;} else {tab_3[6+7*i-70] = 0;}
+						}
+					}
+				}
+				
+				for (int j=0; j<8; j++)
+				{
+					display_complexe(tab_1, tab_2, tab_3, del_1, del_2, del_3);
+				}
+			}
+			else
+			{
+				display_complexe(char_null, char_null, char_null, del_1, del_2, del_3);
+			}
+			break;
+	}
+} 
+
+void display (int table[], int del[])
+{ 
+	for (int i=0;i<35;i++)
+	{	 
+		if (table[i] == 1) {digitalWrite(del[i%7], HIGH);} else {digitalWrite(del[i%7], LOW);}
+		
+		if (i%7 == 6)
+		{
+			digitalWrite(del[6+(i+1)/7], LOW);
+			delay(1);
+			digitalWrite(del[6+(i+1)/7], HIGH);
+		}
+	}
+}
+
+void display_complexe (int table_1[], int table_2[], int table_3[], int del_1[], int del_2[], int del_3[])
+{ 
+	for (int i=0;i<35;i++)
+	{	 
+		if (table_1[i] == 1) {digitalWrite(del_1[i%7], HIGH);} else {digitalWrite(del_1[i%7], LOW);}
+		if (table_2[i] == 1) {digitalWrite(del_2[i%7], HIGH);} else {digitalWrite(del_2[i%7], LOW);}
+		if (table_3[i] == 1) {digitalWrite(del_3[i%7], HIGH);} else {digitalWrite(del_3[i%7], LOW);}
+		
+		if (i%7 == 6)
+		{
+			digitalWrite(del_1[6+(i+1)/7], LOW);
+			digitalWrite(del_2[6+(i+1)/7], LOW);
+			digitalWrite(del_3[6+(i+1)/7], LOW);
+			delay(1);
+			digitalWrite(del_1[6+(i+1)/7], HIGH);
+			digitalWrite(del_2[6+(i+1)/7], HIGH);
+			digitalWrite(del_3[6+(i+1)/7], HIGH);
+		}
+	}
+}
+
+void display_multiple (int table[], int del_1[], int del_2[], int del_3[])
+{ 
+	for (int i=0;i<35;i++)
+	{ 
+		if (table[i] == 1)
+		{
+			digitalWrite(del_1[i%7], HIGH);
+			digitalWrite(del_2[i%7], HIGH);
+			digitalWrite(del_3[i%7], HIGH);
+		}
+		else
+		{
+			digitalWrite(del_1[i%7], LOW);
+			digitalWrite(del_2[i%7], LOW);
+			digitalWrite(del_3[i%7], LOW);
+		}
+		
+		if (i%7 == 6)
+		{
+			digitalWrite(del_1[6+(i+1)/7], LOW);
+			digitalWrite(del_2[6+(i+1)/7], LOW);
+			digitalWrite(del_3[6+(i+1)/7], LOW);
+			delay(2);
+			digitalWrite(del_1[6+(i+1)/7], HIGH);
+			digitalWrite(del_2[6+(i+1)/7], HIGH);
+			digitalWrite(del_3[6+(i+1)/7], HIGH);
+		}
+	}
+}
+
+void display_shift(int table[], int del_1[], int del_2[], int del_3[], int shift)
+{
+	int table_shift[35];
+	
+	for (int i=0; i<35; i++)
+	{
+		table_shift[i] = table[(i+shift*7)%35];
+	}
+	
+	display_multiple(table_shift, del_1, del_2, del_3);
+}
+
+void combine(int table[], int add[])
+{
+	for (int i=0; i<35; i++)
+	{
+		if (table[i] == 0)
+		{
+			table[i] = add[i];
+		}
+	}
+}
+
+void shift(int table[], int table_shift[], int shift)
+{
+	int j;
+	
+	for (int i=0; i<35; i++)
+	{
+		table_shift[i] = 0;
+		
+		if ((i+shift*7) < 35 && (i+shift*7) >=0)
+		{
+			j = table[(i+shift*7)];
+		}
+		else
+		{
+			j = 0;
+		}
+		
+		table_shift[i] = j;
+	}
+}
+
+void display_shift_letter(int * table[], int * del[], int shift, int length)
+{
+	int table_shift[3][35];
+	
+	for (int i=0; i<3; i++)
+	{
+		for (int k=0; k<35; k++)
+		{
+			if((7*shift)%35 <= k)
+			{
+				table_shift[i][k]=table[i+(int)floor(shift/5)][k-(7*shift)%35];
+			}
+			else
+			{
+				table_shift[i][k]=table[i+(int)floor(shift/5)+1][(k-(7*shift)%35)+35];
+			}
+		}
+	}
+	display_complexe(table_shift[0],table_shift[1],table_shift[2],del[0],del[1],del[2]);
+}
+
+void display_shift_words(int * table[], int * del[], int length)
+{
+
+	int shift;
+	
+	for (int i=0;i<((length-1)*5);i++)
+	{
+		for (int j=0;j<10;j++)
+		{
+			if (digitalRead(pinButton) == HIGH && buttonState == LOW)
+			{
+				mode = (mode + 1) % 3;
+				buttonState = HIGH;
+				return;
+			
+			}
+			else if (digitalRead(pinButton) == LOW && buttonState == HIGH)
+			{
+				buttonState = LOW;
+			}
+			display_shift_letter(table,del, i,length);
+		}
+	}
+}
